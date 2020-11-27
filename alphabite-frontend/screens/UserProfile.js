135e1521a542cf69669035e76dbff73f60c80203
Neil Malu <neilmmalu@users.noreply.github.com>
2020-11-27 16:00:56 -0500

    Modified UserProfile.js

--- alphabite-frontend/screens/UserProfile.js
+++ alphabite-frontend/screens/UserProfile.js
@@ -1,8 +1,33 @@
 import React from 'react';
+import { StyleSheet, Text, View, SafeAreaView, Button, Image } from 'react-native';
+
+
+
+const styles = StyleSheet.create({
+    body: {},
+    userImage: {
+        borderColor: '#FFF',
+        borderRadius: 85,
+        borderWidth: 3,
+        height: 170,
+        marginBottom: 15,
+        width: 170,
+    },
+
+})
 
 
 class UserProfile extends React.Component{
     render(){
+        return(
+            <View>
+            <Text>Hi, Vindhya</Text>
+            <Image style={styles.userImage}></Image>
 
+
+            </View>
+        );
     }
 }
+
+export default UserProfile;
